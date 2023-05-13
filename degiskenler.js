ded23a9849204b6a14ffbe8cb55a6fd4909423fd
YaseminDenizOzturk <yasmindeniz188@gmail.com>
2023-05-13 17:47:16 +0300

    upload

---
+++ degiskenler.js
@@ -0,0 +1,46 @@
+console.log("Hello World");
+var yase="yasemin";
+console.log(yase);
+var maasDeniz = 10000;
+var maasYagmur = 4000;
+var zam = 0.50;
+
+console.log(maasDeniz + (maasDeniz * zam))
+console.log(maasYagmur + (maasYagmur * zam))
+
+console.log(10000 + (10000 * 0.5))
+console.log(4000 + (4000 * 0.3))
+
+let ogrenciAdi ="Elif"; 
+let ogrenciNo = 100;
+
+console.log(typeof ogrenciAdi);
+console.log(typeof ogrenciNo);
+
+let number1 = "1";
+let number2 = "3";
+let number3 = "6";
+
+console.log(Number(number1) + Number(number2) + Number(number3));
+
+let x = 120;
+let y = 340;
+
+console.log(x.toString() + y.toString());
+
+let ad = "yasemin";
+let soyad = "öztürk";
+
+console.log(ad + " " + soyad);
+let fiyat = 650;
+
+let ucuzmu = (fiyat <= 1000)
+console.log(ucuzmu);
+
+let yas;
+// hiçbir değer atamadığım zaman undefined ama yas = ""; dersem undefined olmaz string olur.
+console.log(yas);
+console.log(typeof yas);
+
+let degerVar = (yas == undefined)
+console.log(degerVar);
