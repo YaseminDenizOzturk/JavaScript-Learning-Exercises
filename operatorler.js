d78b1430a9b08a828121eb49e5b5a8f37250907e
YaseminDenizOzturk <yasmindeniz188@gmail.com>
2023-05-13 22:19:15 +0300

    upload

---
+++ operatorler.js
@@ -0,0 +1,47 @@
+let result;
+
+let x = 25, y = 40, z = 90;
+
+
+result = x + y;
+result = x - y;
+result = x * y;
+result = x / y;
+result = z % x;
+
+result = x++;
+result = ++x;
+result = --x;
+result = x--;
+
+result = x;
+result += x; // result = result + x
+
+// result ile x arasındaki işlemi gerçekleştir.
+
+result += x;
+result -= x;
+result *= x;
+result /= x;
+result %= x;
+
+result *= (x + y);
+
+
+console.log(result);
+console.log(x);
+
+result = (x == y) // eşitse true 
+result = (x != y) // eşitse false
+result = (5 === "5") // üç eşittir javascriptte iki eşittire ek olarak tip kontrolü de yapar.
+// mesela bu örnek false çıktısı verir .  number ile string birbirine eşit değildir.
+
+result = (x > y) // x, y den küçüktür (false) 
+
+
+
+
+
+
+
+
